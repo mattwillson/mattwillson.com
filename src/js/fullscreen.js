bcdb78fa3e8260b4799dc72d1554b71a8c20fdf9
Matt Willson <mattjameswillson@gmail.com>
2019-05-23 19:26:31 -0400

    Integrate Fscreen library

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -1,36 +1,55 @@
-// Convert HTMLCollection of images within document to an array
-const imgArray = Array.prototype.slice.call(document.images);
+import fscreen from 'fscreen';
+
+// convert HTMLCollection of images within document to an array
+const imgArray = [...document.images];
 
 const toggleFullscreen = e => {
-  // If full-screen mode is supported
-  if (document.fullscreenEnabled) {
 
-    // If full-screen mode is disabled
-    if (!document.fullscreenElement) {
+  // if full-screen mode is supported
+  if (fscreen.fullscreenEnabled) {
 
-      // Enable full-screen mode for image that was clicked
-      e.target.requestFullscreen().catch(err => {
+    // if full-screen mode is disabled
+    if (!fscreen.fullscreenElement) {
+      
+      /**
+       * for Safari compatibility:
+       * remove class from clicked image
+       */
+      e.target.removeAttribute('class');
+
+      // enable full-screen mode for clicked image
+      fscreen.requestFullscreen(e.target).catch(err => {
         alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
       });
 
-    // If full-screen mode is enabled
+    // if full-screen mode is enabled
     } else {
 
-      // Disable full-screen mode
-      document.exitFullscreen().catch(err => {
+      /**
+       * for Safari compatibility:
+       * add class back to clicked image
+       */
+      if (e.target.alt === 'Blouberg Beach' || e.target.alt === 'Alston') {
+        e.target.setAttribute('class', 'photo--landscape');
+      } else {
+        e.target.setAttribute('class', 'photo--square');
+      }
+
+      // disable full-screen mode
+      fscreen.exitFullscreen().catch(err => {
         alert(`Error attempting to disable full-screen mode: ${err.message} (${err.name})`);
       });
     }
   }
 };
 
-// Toggle full-screen mode when an image is clicked
+// toggle full-screen mode when an image is clicked
 imgArray.forEach(img => {
   img.addEventListener('click', toggleFullscreen, false);
 });
 
-document.addEventListener('fullscreenchange', e => {
-  if (document.fullscreenElement) {
+fscreen.addEventListener('fullscreenchange', e => {
+  if (fscreen.fullscreenElement) {
     console.log('Enter full-screen');
   } else {
     console.log('Exit full-screen');
