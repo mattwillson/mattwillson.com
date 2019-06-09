8c40717a897dcf63d19e57a2313f6541553801a2
Matt Willson <mattjameswillson@gmail.com>
2019-06-09 18:06:37 -0400

    Add spacing

--- test/unit/splash_text_set_correctly.js
+++ test/unit/splash_text_set_correctly.js
@@ -6,6 +6,7 @@ describe('Splash text', function() {
   it('should be - Software engineer based<br>in Raleigh, North Carolina', function(done) {
     JSDOM.fromURL('http://localhost:8080/').then(dom => {
       const splashText = dom.window.document.querySelector('body > div.splash > p').innerHTML;
+      
       expect(splashText).to.equal('Software engineer based<br>in Raleigh, North Carolina');
       done();
     }).catch(err => done(err));
