a54e5cfdad16d96b9a104dfa4696abdea41a3e95
Matt Willson <mattjameswillson@gmail.com>
2019-06-09 20:25:51 -0400

    Add unit tests for src attributes

--- test/unit/photos_set_correctly.js
+++ test/unit/photos_set_correctly.js
@@ -25,6 +25,17 @@ describe('Photos', function() {
     });
   });
 
+  describe('Blouberg Beach src', function() {
+    it('should be - 0c44b135d32f9b719bd40634d6b30cef.jpg', function(done) {
+      JSDOM.fromURL('http://localhost:8080/').then(dom => {
+        const bloubergSrc = dom.window.document.getElementsByClassName('photo--landscape')[0].getAttribute('src');
+
+        expect(bloubergSrc).to.equal('0c44b135d32f9b719bd40634d6b30cef.jpg');
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
   describe('Appalachian Trail alt', function() {
     it('should be - Appalachian Trail', function(done) {
       JSDOM.fromURL('http://localhost:8080/').then(dom => {
@@ -41,6 +52,17 @@ describe('Photos', function() {
         const appalachianCaption = dom.window.document.getElementsByClassName('photo')[1].lastElementChild.innerHTML;
 
         expect(appalachianCaption).to.equal('Appalachian Trail, Whitetop, Virginia');
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
+  describe('Appalachian Trail src', function() {
+    it('should be - 8516160f53939b1e1becc249a9e393fd.jpg', function(done) {
+      JSDOM.fromURL('http://localhost:8080/').then(dom => {
+        const appalachianSrc = dom.window.document.getElementsByClassName('photo--square')[0].getAttribute('src');
+
+        expect(appalachianSrc).to.equal('8516160f53939b1e1becc249a9e393fd.jpg');
         done();
       }).catch(err => done(err));
     });
@@ -68,6 +90,17 @@ describe('Photos', function() {
     });
   });
 
+  describe('Alston src', function() {
+    it('should be - 1c1893a52cfa0838411933cfd3068d2c.jpg', function(done) {
+      JSDOM.fromURL('http://localhost:8080/').then(dom => {
+        const alstonSrc = dom.window.document.getElementsByClassName('photo--landscape')[1].getAttribute('src');
+
+        expect(alstonSrc).to.equal('1c1893a52cfa0838411933cfd3068d2c.jpg');
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
   describe('Belle alt', function() {
     it('should be - Belle', function(done) {
       JSDOM.fromURL('http://localhost:8080/').then(dom => {
@@ -85,6 +118,17 @@ describe('Photos', function() {
         const belleCaption = dom.window.document.getElementsByClassName('photo')[3].lastElementChild.innerHTML;
 
         expect(belleCaption).to.equal('Belle, Pittsboro, North Carolina');
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
+  describe('Belle src', function() {
+    it('should be - 146f6202b8cf3b95bd089dbf3144f946.jpg', function(done) {
+      JSDOM.fromURL('http://localhost:8080/').then(dom => {
+        const belleSrc = dom.window.document.getElementsByClassName('photo--square')[1].getAttribute('src');
+
+        expect(belleSrc).to.equal('146f6202b8cf3b95bd089dbf3144f946.jpg');
         done();
       }).catch(err => done(err));
     });
